81cf2108620d24637402f641b4da2bd8343922e7
José Gabriel <jg.barros.dsantos@gmail.com>
2025-11-07 21:24:14 -0300

    vercel ignore fix

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 import app from './src/index.js';
+import './vercel-ignore-fix.js';
+
 
 export default app;
---
+++ vercel-ignore-fix.js
@@ -0,0 +1,3 @@
+// Força inclusão do pg no bundle da Vercel
+import 'pg';
+export default {};
